e297a6097e62c684e540fec8647cb8f44f92080d
aeiou-L <1790912500@qq.com>
2020-11-13 11:07:25 +0800

    On branch element1  Your branch is ahead of 'origin/element1' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.vue
    	modified:   src/main.js
    	new file:   src/plugins/element.js
    	modified:   src/router/index.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
 
 </style>
